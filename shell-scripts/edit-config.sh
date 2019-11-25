43f771c2e933bd2977710a5d52f9a8250445ca77
ftc9881 <ftc9881@gmail.com>
2019-11-25 12:00:30 -0800

    move configs

--- shell-scripts/edit-config.sh
+++ shell-scripts/edit-config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 CONFIG_FILE=$1
-~/Android/Sdk/platform-tools/adb pull sdcard/Robot/$CONFIG_FILE
-vim $CONFIG_FILE
-~/Android/Sdk/platform-tools/adb push $CONFIG_FILE sdcard/Robot/$CONFIG_FILE
+$ANDROID_SDK_HOME/platform-tools/adb pull sdcard/Robot/$CONFIG_FILE
+vim ../configs/$CONFIG_FILE
+$ANDROID_SDK_HOME/platform-tools/adb push ../configs/$CONFIG_FILE sdcard/Robot/$CONFIG_FILE
