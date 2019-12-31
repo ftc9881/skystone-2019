1c1efaaded7f913ba999451c9ea95af992c33de6
ftc9881 <ftc9881@gmail.com>
2019-12-31 15:50:52 -0800

    quick reference for block detection driver

--- ftc-app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/SensorTest.java
+++ ftc-app/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/SensorTest.java
@@ -7,6 +7,7 @@ package org.firstinspires.ftc.teamcode.teleop.opmodes;
 import org.firstinspires.ftc.teamcode.sensors.MaxSonarI2CXL;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 import org.firstinspires.ftc.teamcode.sensors.SharpDistanceSensor;
 import org.firstinspires.ftc.teamcode.teleop.utility.TeleOpBase;
@@ -14,22 +15,30 @@ import org.firstinspires.ftc.teamcode.teleop.utility.TeleOpBase;
 @TeleOp
 public class SensorTest extends TeleOpBase {
     private SharpDistanceSensor sharpDistanceSensor;
-    private MaxSonarI2CXL sonarTest;
+    private MaxSonarI2CXL sonarSensor;
+    private DigitalChannel digitalDistanceSensor;
 
     @Override
     protected void initialize() {
-        AnalogInput sharpAnalogInput = robot.hardwareMap.analogInput.get("sharp");
-        sharpDistanceSensor = new SharpDistanceSensor(sharpAnalogInput);
-//        sonarTest = robot.hardwareMap.get(MaxSonarI2CXL.class, "sonar");
-//        sonarTest.startAutoPing(100);
+
+        digitalDistanceSensor = robot.hardwareMap.digitalChannel.get("digital");
+
+//        AnalogInput sharpAnalogInput = robot.hardwareMap.analogInput.get("sharp");
+//        sharpDistanceSensor = new SharpDistanceSensor(sharpAnalogInput);
+
+//        sonarSensor = robot.hardwareMap.get(MaxSonarI2CXL.class, "sonar");
+//        sonarSensor.startAutoPing(100);
+
     }
 
     @Override
     protected void update() {
-        telemetry.addData("IR-LED Sensor Distance: ", sharpDistanceSensor.getDistance());
+        telemetry.addData("Digital", digitalDistanceSensor.getState());
 
-//        telemetry.addData("Sonar Connection Info", sonarTest.getConnectionInfo());
-//        telemetry.addData("Sonar Distance: ", sonarTest.getDistance());
+//        telemetry.addData("IR-LED Sensor Distance: ", sharpDistanceSensor.getDistance());
+
+//        telemetry.addData("Sonar Connection Info", sonarSensor.getConnectionInfo());
+//        telemetry.addData("Sonar Distance: ", sonarSensor.getDistance());
 
         telemetry.update();
     }
